c1af7514fe2b6edb78b26488981607d0849d8102
Cheezyrock <cheezyrock@gmail.com>
2023-02-03 21:16:56 -0600

    Added Basic GameEventManager

---
+++ Assets/Code/GameEventManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEventManager : MonoBehaviour
+{
+    public GameEventManager Instance { get; private set; }
+
+    bool isPaused = false;
+
+    bool isQuitting = false;
+    public GameObject confirmationUI;
+    public AudioSource pauseAudioSource;
+
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            isPaused = !isPaused;
+            if (pauseAudioSource != null)
+            {
+                if (isPaused)
+                {
+                    SoundManager.Instance.bgmMusicManager.Pause();
+                    pauseAudioSource.Play();
+                }
+                else
+                {
+                    pauseAudioSource.Stop();
+                    SoundManager.Instance.bgmMusicManager.UnPause();
+                }
+            }
+
+            Time.timeScale = (isPaused ? 0 : 1);
+        }
+
+        if (Input.GetButtonDown("Quit"))
+        {
+            isQuitting = !isQuitting;
+
+            if (isQuitting && confirmationUI == null)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                confirmationUI.SetActive(isQuitting);
+            }
+        }
+
+        if (isQuitting)
+        {
+            // Do menu navigation for buttons
+        }
+        
+
+    }
+
+}
