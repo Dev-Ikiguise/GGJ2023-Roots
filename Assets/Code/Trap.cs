044974aa48652fd0385453c69435f0f58e326dc2
WUStudent <wustudent@webster.edu>
2023-02-04 19:51:39 -0600

    yes

--- Assets/Code/Trap.cs
+++ Assets/Code/Trap.cs
@@ -7,46 +7,64 @@ public class Trap : MonoBehaviour
 {
     public float UpwardVelocity;
 
+    bool canLaunch;
+    public GameObject closedTrap;
+    public GameObject openedTrap;
+
+    private void Start()
+    {
+        canLaunch = true;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         //find what is hitting us, find rigid body, apply upwards force
-
-        print(collision.gameObject.name);
-
         if (collision.gameObject.GetComponent<NavMeshAgent>())
         {
             collision.gameObject.GetComponent<NavMeshAgent>().enabled = false;
         }
-        collision.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * UpwardVelocity);
+        if (collision.gameObject.GetComponent<Rigidbody>() && canLaunch)
+        {
+            Launch(collision.gameObject.GetComponent<Rigidbody>());
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         //find what is hitting us, find rigid body, apply upwards force
-
-        Vector3 initialVelocity;
-
-        print(other.gameObject.name);
-
         if (other.gameObject.GetComponent<NavMeshAgent>())
         {
             other.gameObject.GetComponent<NavMeshAgent>().enabled = false;
             
         }
-        if (other.gameObject.GetComponent<Rigidbody>())
+        if (other.gameObject.GetComponent<Rigidbody>() && canLaunch)
         {
-            other.gameObject.GetComponent<Rigidbody>().isKinematic = false;
-
-            float randX = Random.Range(-20, 20);
-            float randZ = Random.Range(-20, 20);
-            Vector3 randomVector3 = new Vector3(randX, randZ, randZ);
-
-            other.gameObject.GetComponent<Rigidbody>().AddForce((Vector3.up * UpwardVelocity) + randomVector3);
-            other.gameObject.GetComponent<Rigidbody>().AddTorque(randomVector3);
+            Launch(other.gameObject.GetComponent<Rigidbody>());
         }
-        //if (other.gameObject.GetComponent<EnemyHealth>())
-        //{
-        //    other.gameObject.GetComponent<EnemyHealth>().HandleDeath();
-        //}
+    }
+
+    void Launch(Rigidbody rigidbody)
+    {
+        rigidbody.isKinematic = false;
+
+        float randX = Random.Range(-20, 20);
+        float randZ = Random.Range(-20, 20);
+        Vector3 randomVector3 = new Vector3(randX, randZ, randZ);
+
+        rigidbody.AddForce((Vector3.up * UpwardVelocity) + randomVector3);
+        rigidbody.AddTorque(randomVector3);
+
+        StartCoroutine(AnimateCo());
+    }
+
+    private IEnumerator AnimateCo()
+    {
+        canLaunch = false;
+        closedTrap.SetActive(false);
+        openedTrap.SetActive(true);
+        yield return new WaitForSeconds(3);
+        closedTrap.SetActive(true);
+        openedTrap.SetActive(false);
+        canLaunch = true;
     }
 }
